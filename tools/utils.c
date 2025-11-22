da1a1f6e98132246c1c350ac7a5b38a3a00de97a
gdpps1028 <gdpps1028.cs12@nycu.edu.tw>
2025-11-23 02:45:49 +0800

    updated printf for more formatting support

--- tools/utils.c
+++ tools/utils.c
@@ -4,8 +4,10 @@ void printf(char *format, ...) {
     char *idx;
 
     bool is_special_code = false;
-    bool should_print_preleading_zeroes = false;
-    bool printed_anything = false;
+    bool zero_padding_checked = false;
+    bool is_zero_padded = false;
+    bool is_left_aligned = false;
+    uint8_t field_width = 0;
 
     uint8_t arg_char_counter = 0;
 
@@ -14,49 +16,90 @@ void printf(char *format, ...) {
 
     for (idx = format; *idx != NULL; idx++) {
         if (is_special_code) {
+
+            if (*idx == '-') {
+                // Handle left alignment
+                is_left_aligned = true;
+                continue;
+            }
+
+            if (*idx == '0' && !zero_padding_checked) {
+                // Handle zero-padding
+                is_zero_padded = true;
+                zero_padding_checked = true;
+                continue;
+            }
+
+            if (*idx >= '0' && *idx <= '9') {
+                // Handle the width
+                field_width = field_width * 10 + (*idx - '0');
+                zero_padding_checked = true;
+                continue;
+            }
+
             if (*idx != 'c' && *idx != 'C') { arg_char_counter = 0; }
             if (*idx == 'x' || *idx == 'X') {
                 // Treat *arg as a hex number
                 uint32_t arg_val = va_arg(ap, uint32_t);
                 bool has_seen_nonzero = false;
                 uint32_t counter = 8; // Max length
+                char hex_str[8];      // Hex string for the number
+                uint8_t hex_idx = 0;
                 while (counter > 0) {
                     int cur_digit = ((arg_val & 0xF0000000) >> 28);
                     if (cur_digit != 0) has_seen_nonzero = true;
 
-                    if ((!(cur_digit == 0 && !has_seen_nonzero)) || should_print_preleading_zeroes) {
+                    if ((!(cur_digit == 0 && !has_seen_nonzero))) {
                         if (cur_digit < 0x0A) {
                             // Numeric
-                            serial_putc(cur_digit + 0x30);
+                            // serial_putc(cur_digit + 0x30);
+                            hex_str[hex_idx++] = (cur_digit + 0x30);
                         }
                         else {
                             // Alpha
-                            serial_putc(cur_digit + 0x41 - 0x0A);
+                            // serial_putc(cur_digit + 0x41 - 0x0A);
+                            if (*idx == 'X') {
+                                hex_str[hex_idx++] = (cur_digit + 0x41 - 0x0A);
+                            }
+                            else {
+                                hex_str[hex_idx++] = (cur_digit + 0x41 - 0x0A - 'A' + 'a');
+                            }
                         }
-                        printed_anything = true;
                     }
 
                     arg_val = arg_val << 4;
                     counter--;
                 }
 
-                if (!printed_anything) {
+                if (hex_idx == 0) {
                     // Thing to print was 0 and we didn't enable preleading zeroes
                     // So just print a 0 and move on
-                    serial_putc('0');
+                    // serial_putc('0');
+                    hex_str[hex_idx++] = '0';
+                }
+
+                int i;
+                if (field_width && !is_left_aligned) {
+                    for (i = field_width - 1; i >= hex_idx; i--) {
+                        if (is_zero_padded) {
+                            serial_putc('0');
+                        }
+                        else {
+                            serial_putc(' ');
+                        }
+                    }
+                }
+                for (i = 0; i < hex_idx; i++) {
+                    serial_putc(hex_str[i]);
+                }
+                if (field_width && is_left_aligned) {
+                    for (i = field_width - 1; i >= hex_idx; i--) {
+                        serial_putc(' ');
+                    }
                 }
 
                 // Move to next argument:
                 is_special_code = false;
-                should_print_preleading_zeroes = false;
-                printed_anything = false;
-                continue;
-            }
-            else if (*idx == '0') {
-                // This means preprint the leading zeroes
-                should_print_preleading_zeroes = true;
-
-                // Not done with this argument yet so continue:
                 continue;
             }
             else if (*idx == 's' || *idx == 'S') {
@@ -67,7 +110,6 @@ void printf(char *format, ...) {
                     cursor++;
                 }
 
-                should_print_preleading_zeroes = false;
                 is_special_code = false;
                 continue;
             }
@@ -82,12 +124,13 @@ void printf(char *format, ...) {
             else if (*idx == 'd' || *idx == 'D') {
                 // Treat *arg as a decimal integer
                 int32_t arg_val = va_arg(ap, int);
+                bool is_neg = false;
                 if (arg_val < 0) {
                     // Print the negative sign
                     serial_putc('-');
+                    is_neg = true;
                     arg_val = -arg_val; // Make it positive for printing
                 }
-                
                 // Convert integer to string and print
                 char num_str[11]; // Max 10 digits for 32-bit int + 1 for null terminator
                 uint8_t num_idx = 0;
@@ -103,8 +146,23 @@ void printf(char *format, ...) {
 
                 // Print digits in reverse order
                 int i;
+                if (field_width && !is_left_aligned) {
+                    for (i = field_width - 1 - is_neg; i >= num_idx; i--) {
+                        if (is_zero_padded) {
+                            serial_putc('0');
+                        }
+                        else {
+                            serial_putc(' ');
+                        }
+                    }
+                }
                 for (i = num_idx - 1; i >= 0; i--) {
                     serial_putc(num_str[i]);
+                }
+                if (field_width && is_left_aligned) {
+                    for (i = field_width - 1 - is_neg; i >= num_idx; i--) {
+                        serial_putc(' ');
+                    }
                 }
 
                 is_special_code = false;
